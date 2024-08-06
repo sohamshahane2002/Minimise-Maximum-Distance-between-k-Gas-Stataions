fd5d34330d23d9813722316153dafa742e59c3ba
sohamshahane2002 <134382971+sohamshahane2002@users.noreply.github.com>
2024-08-07 01:00:01 +0530

    Add files via upload

---
+++ min_max_dist_betn_gasStation.java
@@ -0,0 +1,45 @@
+package BinarySearchProblems.BinarySearch8;
+public class min_max_dist_betn_gasStation{
+    public static int numberOfGasStationsRequired(double dist, int[] arr) {
+        int n = arr.length; // 
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            int numberInBetween = (int)((arr[i] - arr[i - 1]) / dist);
+            if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+                numberInBetween--;
+            }
+            cnt += numberInBetween;
+        }
+        return cnt;
+    }
+
+    public static double minimiseMaxDistance(int[] arr, int k) {
+        int n = arr.length; 
+        double low = 0;
+        double high = 0;
+
+        //Find the maximum distance:
+        for (int i = 0; i < n - 1; i++) {
+            high = Math.max(high, (double)(arr[i + 1] - arr[i]));
+        }
+        //Apply Binary search:
+        double diff = 1e-6 ;
+        while (high - low > diff) {
+            double mid = (low + high) / (2.0);
+            int cnt = numberOfGasStationsRequired(mid, arr);
+            if (cnt > k) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return high;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 4;
+        double ans = minimiseMaxDistance(arr, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
---
+++ min_max_dist_betn_gasStation_.java
@@ -0,0 +1,44 @@
+package BinarySearchProblems.BinarySearch8;
+
+public class min_max_dist_betn_gasStation_ {
+    public static double minimiseMaxDistance(int[] arr, int k) {
+        int n = arr.length; //size of array.
+        int[] howMany = new int[n - 1];
+
+        //Pick and place k gas stations:
+        for (int gasStations = 1; gasStations <= k; gasStations++) {
+            //Find the maximum section
+            //and insert the gas station:
+            double maxSection = -1;
+            int maxInd = -1;
+            for (int i = 0; i < n - 1; i++) {
+                double diff = arr[i + 1] - arr[i];
+                double sectionLength =
+                    diff / (double)(howMany[i] + 1);
+                if (sectionLength > maxSection) {
+                    maxSection = sectionLength;
+                    maxInd = i;
+                }
+            }
+            //insert the current gas station:
+            howMany[maxInd]++;
+        }
+
+        //Find the maximum distance =the answer:
+        double maxAns = -1;
+        for (int i = 0; i < n - 1; i++) {
+            double diff = arr[i + 1] - arr[i];
+            double sectionLength =
+                diff / (double)(howMany[i] + 1);
+            maxAns = Math.max(maxAns, sectionLength);
+        }
+        return maxAns;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 4;
+        double ans = minimiseMaxDistance(arr, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
